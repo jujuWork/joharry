ae6ca17440e71dc1a310e249b876135162c394e8
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2026-01-20 21:05:34 +0900

    linking php files

--- pages/nav.php
+++ pages/nav.php
@@ -1,3 +1,12 @@
 <nav>
-    <h1>Navigation</h1>
+    <div class="nav__container">
+        <ul>
+            <li class="nav__links nav__name">Joharry Sanchez</li>
+            <li class="nav__links">Work Experience</li>
+            <li class="nav__links">Skills</li>
+            <li class="nav__links">Certificates</li>
+            <li class="nav__links">Works</li>
+            <li class="nav__links nav__contact">Contact Me</li>
+        </ul>
+    </div>
 </nav>
