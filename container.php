7bbc1e87e98e9b886c596140d5cee2cecf805b43
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2026-01-20 20:55:31 +0900

    linking php files

--- container.php
+++ container.php
@@ -5,15 +5,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Portfolio</title>
 
-<?php
-get_header();
-?>
+    <!-- MAIN CSS -->
+    <link rel="stylesheet" href="/style.css">
+
+    <!-- NAVIGATION CSS -->
+     <link rel="stylesheet" href="./assets/css/nav.css">
 
 </head>
 <body>
 
 <div class="grid_container">
-    <div class="grid_item header">header</div>
-    <div class="grid_item main">main</div>
-    <div class="grid_item footer">footer</div>
+    <div class="grid_item header">
+        <?php include './pages/nav.php' ?>
+        <?php include 'header.php' ?>
+    </div>
+    <div class="grid_item main">
+        <?php include 'main.php' ?>
+    </div>
+    <div class="grid_item footer">
+        <?php include 'footer.php' ?>
+    </div>
 </div>
--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php
 
-require '/container.php';
+require 'container.php';
