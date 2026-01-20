688aab418c9e0a432f4f8b32ea9190858287b581
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2026-01-20 21:13:10 +0900

    navigation

--- pages/nav.php
+++ pages/nav.php
@@ -1,6 +1,6 @@
 <nav>
     <div class="nav__container">
-        <ul>
+        <ul class="nav__list">
             <li class="nav__links nav__name">Joharry Sanchez</li>
             <li class="nav__links">Work Experience</li>
             <li class="nav__links">Skills</li>
