1a3522668242ac43946b0939f3a8ee95a6c0cb5d
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2026-01-22 23:48:02 +0900

    hamburger menu

--- container.php
+++ container.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,26 +10,25 @@
     <link rel="stylesheet" href="/style.css">
 
     <!-- NAVIGATION STYLES -->
-     <link rel="stylesheet" href="./assets/css/nav.css">
+    <link rel="stylesheet" href="./assets/css/nav.css">
 
-     <!-- HEADER STYLES -->
-      <link rel="stylesheet" href="./assets/css/header.css">
+    <!-- HEADER STYLES -->
+    <link rel="stylesheet" href="./assets/css/header.css">
 
 </head>
+
 <body>
 
-<div class="grid_container">
-    <div class="grid_item header">
-        <div class="nav">
+    <div class="grid_container">
+        <div class="grid_item header">
             <?php include './pages/nav.php' ?>
-        </div>
-        <?php include 'header.php' ?>
+            <?php include 'header.php' ?>
 
+        </div>
+        <div class="grid_item main">
+            <?php include 'main.php' ?>
+        </div>
+        <div class="grid_item footer">
+            <?php include 'footer.php' ?>
+        </div>
     </div>
-    <div class="grid_item main">
-        <?php include 'main.php' ?>
-    </div>
-    <div class="grid_item footer">
-        <?php include 'footer.php' ?>
-    </div>
-</div>
--- pages/nav.php
+++ pages/nav.php
@@ -1,22 +1,37 @@
 <nav>
-    <ul class="nav__list">
+    <div class="burger__wrapper">
+        <label for="burger" class="burger">
+            <input type="checkbox" id="burger">
+            <span></span>
+            <span></span>
+            <span></span>
+        </label>
+    </div>
 
-        <li class="nav__name">
-            <a href="#home">Joharry Sanchez</a>
-        </li>
-        <li class="nav__links">
-            <a href="#work">Work Experience</a>
-        </li>
-        <li class="nav__links">
-            <a href="#skills">Skills</a>
-        </li>
-        <li class="nav__links">
-            <a href="#certificate">Certificate</a>
-        </li>
-        <li class="nav__links">
-            <a href="#works">Works</a>
-        </li>
 
-        <button class="nav__contact">Contact Me</button>
-    </ul>
+
+
+
+
+
+    <div class="nav__list">
+        <ul class="nav__link--wrapper">
+            <li class="nav__name">
+                <a href="#home">Joharry Sanchez</a>
+            </li>
+            <li class="nav__links">
+                <a href="#work">Work Experience</a>
+            </li>
+            <li class="nav__links">
+                <a href="#skills">Skills</a>
+            </li>
+            <li class="nav__links">
+                <a href="#certificate">Certificate</a>
+            </li>
+            <li class="nav__links">
+                <a href="#works">Works</a>
+            </li>
+            <button class="nav__contact">Contact Me</button>
+        </ul>
+    </div>
 </nav>
