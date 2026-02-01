d609a279cb9e5f92b4e5d4f75ec8f21670548532
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2026-02-01 11:24:26 +0900

    images

---
+++ assets/js/header.js
@@ -0,0 +1,16 @@
+const images = document.querySelectorAll(".header__image-wrapper img");
+let currentIndex = 0;
+
+function nextSlide() {
+  //Fade Out
+  images[currentIndex].classList.remove("active");
+
+  // Increment Index
+  currentIndex = (currentIndex + 1) % images.length;
+
+  //Fade In
+  images[currentIndex].classList.add("active");
+}
+
+// Fade every 2 secs + 0.5s transition
+setInterval(nextSlide, 2500);
