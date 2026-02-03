e905e40d337e0ae56473686ac9dd04c49e602aa3
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2026-02-03 23:24:51 +0900

    navigation bar

--- pages/nav.php
+++ pages/nav.php
@@ -1,4 +1,5 @@
 <nav>
+    <!-- HAMBURGER MENU START -->
     <button class="burger" onclick="toggleMenu()">
         <span></span>
         <span></span>
@@ -30,6 +31,34 @@
             </li>
 
             <button class="burger__menu-join">Join Us</button>
+        </ul>
+    </div>
+    <!-- HAMBURGER MENU END -->
+
+    <!-- Laptop and Up navigation -->
+    <div class="nav__container">
+        <ul class="nav__menu">
+            <li class="nav-items">
+                <a href="#work" class="nav__links">Home</a>
+            </li>
+            <li class="nav-items">
+                <a href="#about" class="nav__links">About</a>
+            </li>
+            <li class="nav-items">
+                <a href="#price" class="nav__links">Pricing</a>
+            </li>
+            <li class="nav-items">
+                <a href="#gallery" class="nav__links">Gallery</a>
+            </li>
+            <li class="nav-items">
+                <a href="#page" class="nav__links">Pages</a>
+            </li>
+            <li class="nav-items">
+                <a href="#blog" class="nav__links">Blog</a>
+            </li>
+            <li class="nav-items">
+                <a href="#contact" class="nav__links">Contact</a>
+            </li>
         </ul>
     </div>
 </nav>
