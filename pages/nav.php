7d5386eb34fb01f7e8a4b649f35596c018af68e3
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2026-02-04 23:05:27 +0900

    navigation for wide screen

--- pages/nav.php
+++ pages/nav.php
@@ -38,27 +38,35 @@
     <!-- Laptop and Up navigation -->
     <div class="nav__container">
         <ul class="nav__menu">
-            <li class="nav-items">
+            <li class="nav__items">
                 <a href="#work" class="nav__links">Home</a>
             </li>
-            <li class="nav-items">
+            <li class="nav__items">
                 <a href="#about" class="nav__links">About</a>
             </li>
-            <li class="nav-items">
+            <li class="nav__items">
                 <a href="#price" class="nav__links">Pricing</a>
             </li>
-            <li class="nav-items">
+            <li class="nav__items">
                 <a href="#gallery" class="nav__links">Gallery</a>
             </li>
-            <li class="nav-items">
+            <li class="nav__items">
                 <a href="#page" class="nav__links">Pages</a>
             </li>
-            <li class="nav-items">
+            <li class="nav__items">
                 <a href="#blog" class="nav__links">Blog</a>
             </li>
-            <li class="nav-items">
+            <li class="nav__items">
                 <a href="#contact" class="nav__links">Contact</a>
             </li>
+            <li class="nav__items">
+                <div class="header__logo">
+                    <p class="logo__title">
+                        G<span>y</span>m<span>.</span>
+                    </p>
+                </div>
+            </li>
+
         </ul>
     </div>
 </nav>
