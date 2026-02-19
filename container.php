fab58bee46122f06ca25f293dd0079142c81f5af
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2026-02-19 21:33:21 +0900

    main content

--- container.php
+++ container.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Portfolio</title>
+    <title>GYM</title>
 
     <!-- MAIN STYLES -->
     <link rel="stylesheet" href="/style.css">
