ef07112f426d8bdf536099def9657a95108c86f9
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2026-02-19 23:57:04 +0900

    main content

--- footer.php
+++ footer.php
@@ -1,12 +1,13 @@
-<!-- <footer>
+<footer>
     <h1>FOOTER</h1>
-</footer> -->
+</footer>
 
 
 <script>
     <?php
     include 'assets/js/nav.js';
     include 'assets/js/header.js';
+    include 'assets/js/main.js'
     ?>
 </script>
 </body>
--- pages/main.php
+++ pages/main.php
@@ -1,3 +1,10 @@
 <main>
-    
+    <section class="main__container">
+        <div class="main__para-section">
+            <img src="https://images.unsplash.com/photo-1506905925346-21bda4d32df4" alt="img01">
+        </div>
+        <div class="main__para-section">
+            <img src="https://images.unsplash.com/photo-1511593358241-7eea1f3c84e5" alt="img02">
+        </div>
+    </section>
 </main>
