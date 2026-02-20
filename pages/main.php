8c0612525def33dd9bafd2723059201e6dc17cc6
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2026-02-20 22:51:47 +0900

    main_content

--- pages/main.php
+++ pages/main.php
@@ -1,10 +1,28 @@
 <main>
     <section class="main__container">
-        <div class="main__para-section">
-            <img src="https://images.unsplash.com/photo-1506905925346-21bda4d32df4" alt="img01">
+        <div class="main__section section__girl">
+            <section class="section__text">
+                <h3>For Girls</h3>
+                <p>
+                    here are many variations of passages of lorem
+                    Ipsum available, but the majority have suffered
+                    alteration.
+                </p>
+                <button class="hero__btn">
+                    Join Now
+                </button></button>
+            </section>
         </div>
-        <div class="main__para-section">
-            <img src="https://images.unsplash.com/photo-1511593358241-7eea1f3c84e5" alt="img02">
+        <div class="main__section section__boy">
+            <section class="section__text">
+                <h3>For Mens</h3>
+                <p>
+                    here are many variations of passages of lorem
+                    Ipsum available, but the majority have suffered
+                    alteration.
+                </p>
+                <button>Join Us</button>
+            </section>
         </div>
     </section>
 </main>
