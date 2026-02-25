653f0017d4cc093666cb4980741bdd2e84224798
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2026-02-26 00:01:53 +0900

    aside features

--- pages/aside.php
+++ pages/aside.php
@@ -18,6 +18,27 @@
                     There are many variations of passages of lorem Ipsum available.
                 </p>
             </li>
+            <li class="carousel__items">
+                <img src="/assets/images/aside/img02.svg" alt="weightlifting">
+                <h2>Weightlifting</h2>
+                <p>
+                    There are many variations of passages of lorem Ipsum available.
+                </p>
+            </li>
+            <li class="carousel__items">
+                <img src="/assets/images/aside/img03.svg" alt="weightlifting">
+                <h2>Weightlifting</h2>
+                <p>
+                    There are many variations of passages of lorem Ipsum available.
+                </p>
+            </li>
+            <li class="carousel__items">
+                <img src="/assets/images/aside/img04.svg" alt="weightlifting">
+                <h2>Weightlifting</h2>
+                <p>
+                    There are many variations of passages of lorem Ipsum available.
+                </p>
+            </li>
         </ul>
     </section>
 </aside>
