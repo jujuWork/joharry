ddf900f435846d2bb1b9484afa4f4bcf9ee3b607
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2026-02-25 23:33:45 +0900

    aside

--- pages/aside.php
+++ pages/aside.php
@@ -1,3 +1,23 @@
 <aside>
-    FEATUREs
+    <section class="aside__container">
+        <div class="aside__title">
+            <h2>Our Features</h2>
+            <p>
+                There are many variations of passages of lorem Ipsum available, but the majority
+                have suffered alteration.
+            </p>
+        </div>
+    </section>
+
+    <section class="carousel__container">
+        <ul class="carousel__item-wrapper">
+            <li class="carousel__items">
+                <img src="/assets/images/aside/img01.svg" alt="weightlifting">
+                <h2>Weightlifting</h2>
+                <p>
+                    There are many variations of passages of lorem Ipsum available.
+                </p>
+            </li>
+        </ul>
+    </section>
 </aside>
